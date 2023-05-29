b5bb0d73684389a3cecec77f2aab4a2e49e3dd95
Aditya Kuntal <geekysingh2502@gmail.com>
2023-05-29 12:25:22 +0530

    Leetcode-222

---
+++ count_nodes_of_complete_Tree.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void build_tree_LOD(node* &root)
+{
+    int data;
+    cout<<"Enter data for root node of tree"<<endl;
+    cin>>data;
+    queue<node*>q;
+    root = new node(data);
+    if(data == -1) return ;
+    q.push(root);
+    while(!q.empty())
+    {
+        node* temp = q.front();
+        q.pop();
+        
+        cout<<"Enter data for left of "<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata != -1)
+        {
+            temp->left = new node(leftdata);
+            q.push(temp->left);
+        }
+
+         
+        cout<<"Enter data for right of "<<temp->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata != -1)
+        {
+            temp->right = new node(rightdata);
+            q.push(temp->right);
+        }
+    }
+}
+void LOD(node* root)
+{
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty())
+    {
+        node* temp = q.front();
+        q.pop();
+        if(temp == NULL)
+        {
+            cout<<endl;
+            if(!q.empty())q.push(NULL);
+        }
+        else
+        {
+            cout<<temp->data<<" ";
+            if(temp->left)q.push(temp->left);
+            if(temp->right)q.push(temp->right);
+        }
+
+    }
+
+
+
+}
+
+
+int leftheight(node* root)
+{
+    int hgt = 0;
+    while(root!=NULL)
+    {
+        hgt++;
+        root=root->left;
+    }
+    return hgt;
+}
+
+int rightheight(node* root)
+{
+    int hgt = 0;
+    while(root!=NULL)
+    {
+        hgt++;
+        root=root->right;
+    }
+    return hgt;
+}
+
+int nodes(node* root)
+{
+    if(root == NULL)return 0;
+    int lh = leftheight(root);
+    int rh = rightheight(root);
+    if(lh == rh)return (1<<lh)-1;
+    return 1+nodes(root->left)+nodes(root->right);
+}
+
+
+
+
+
+
+
+
+int main()
+{
+    node* root = NULL;
+    
+    build_tree_LOD(root);
+    LOD(root);
+
+    cout<<nodes(root)<<endl;
+
+}
